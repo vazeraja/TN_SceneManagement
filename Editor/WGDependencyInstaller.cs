da4a4d2a6ae7bdc7c32a26c6b91bd39207fb04c4
Surya Tej Kodali <surya.kodali@gmail.com>
2022-08-24 19:51:42 -0400

    fix: add dependency installer

---
+++ Editor/WGDependencyInstaller.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+
+namespace ThunderNut.SceneManagement.Editor
+{
+    public static class WGDependencyInstaller
+    {
+        static AddRequest Request;
+
+        [MenuItem("Tools/Add Package Example")]
+        static void Add()
+        {
+            // Add a package to the project
+            Request = Client.Add("com.unity.textmeshpro");
+            EditorApplication.update += Progress;
+        }
+
+        static void Progress()
+        {
+            if (Request.IsCompleted)
+            {
+                if (Request.Status == StatusCode.Success)
+                    Debug.Log("Installed: " + Request.Result.packageId);
+                else if (Request.Status >= StatusCode.Failure)
+                    Debug.Log(Request.Error.message);
+
+                EditorApplication.update -= Progress;
+            }
+        } 
+    }
+}
